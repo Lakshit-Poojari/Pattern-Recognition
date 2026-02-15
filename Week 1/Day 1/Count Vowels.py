f68536583b8847112b2f4bc1ea80aa2aff20c7c8
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-02-15 23:05:50 +0530

    count vowels

--- Week 1/Day 1/Count Vowels.py
+++ Week 1/Day 1/Count Vowels.py
@@ -1,1 +1,14 @@
 # Count Vowels
+
+word = str(input("Enter a word "))
+
+vowels = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"]
+
+count = 0
+for i in word:
+    if i in vowels:
+        count += 1
+        print(i)
+    # else:
+
+print("Number of vowels are", count)
