8f70ffc77d365bbf183e149b7390ed7bafcf5b4f
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-02-15 23:22:09 +0530

    add sum of digit

--- Week 1/Day 1/SumOfDigits.py
+++ Week 1/Day 1/SumOfDigits.py
@@ -1,1 +1,16 @@
 # Sum of Digits
+
+num = str(int(input("Enter Number")))
+
+m=0
+for i in num:
+    m =int(i)+m
+
+print(f"sum of number is ",m)
+
+
+n=1 
+for i in num:
+    n=n*int(i)
+
+print(f"sum of number is ",n)
