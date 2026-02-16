0a0a6b1125b4bf8bb5059b783329002ecd10bc87
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-02-16 21:26:27 +0530

    changes done

--- Week 1/Day 2/largestNumbersInList.py
+++ Week 1/Day 2/largestNumbersInList.py
@@ -5,11 +5,11 @@ arr_limit = int(input("Enter Number of Element "))
 
 larger_number = []
 num = int(input("Enter Number to be Compared "))
-count= 1
+count = 1
 for i in range(arr_limit):
     ele = int(input(f"Enter Element number {count} "))
     arr.append(ele)
-    count+=1
+    count += 1
 
 print(arr)
 for a in arr:
