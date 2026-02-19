de6c07c7f8f4fbb45bdb1806ccd31bba8d739f4d
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-02-19 17:41:03 +0530

    create count frequency and first non repeating number

--- Week 1/Day 3/CountFrequencyofNumbers.py
+++ Week 1/Day 3/CountFrequencyofNumbers.py
@@ -1,1 +1,21 @@
 # Count Frequency of Numbers
+
+num = int(input("Enter Number of element in list "))
+numbers = []
+frequency = {}
+
+i = 1
+for i in range(num):
+    value = int(input("Enter the value "))
+    numbers.append(value)
+    i+=1
+
+print(numbers)
+
+for j in numbers:
+    if j in frequency:
+        frequency[j] += 1
+    else:
+        frequency[j] = 1
+
+print(frequency) 
--- Week 1/Day 3/FirstNon-RepeatingCharacter.py
+++ Week 1/Day 3/FirstNon-RepeatingCharacter.py
@@ -1,1 +1,19 @@
 # First Non-Repeating Character
+
+word= str(input("Enter a word "))
+
+frequency = {}
+
+for alpha in word:
+    if alpha in frequency:
+        frequency[alpha] += 1
+
+    else:
+        frequency[alpha] = 1
+
+print(frequency)
+
+for alpha in word:
+    if frequency[alpha] == 1:
+        print("First non-repeating character:", alpha)
+        break
