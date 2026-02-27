54772ad1434400abdded6a83c6d7a75b526ca88e
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-02-27 16:48:52 +0530

    push day 5 final code

--- Week 1/Day 5/StringCompression.py
+++ Week 1/Day 5/StringCompression.py
@@ -1,14 +1,15 @@
-# Continues Tracking
+# String Compression 
 
 word = str(input("Enter a word "))
 
 count = 1
+
 for i in range(len(word) -1):
     if word[i] == word[i+1]:
-        count +=1
-        
+        count += 1
+
     else:
-        print(f"{word[i]}{count}")
+        print(f"{word[i]}{count}", end = "")
         count = 1
         
-print(f"{word[-1]}{count}")
+print(f"{word[-1]}{count}", end="")
