8c565714b9a83662bd3cacfa25f90f206848c6fa
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-02-28 13:30:07 +0530

    sliding window

--- Week 1/Day 6/SlidingWindowVowels.py
+++ Week 1/Day 6/SlidingWindowVowels.py
@@ -5,7 +5,6 @@ word = input("Enter a word ").upper()
 vowels = ["A", "E", "I", "O", "U"]
 window = 4
 
-# First window
 count = 0
 for i in range(window):
     if word[i] in vowels:
@@ -13,7 +12,6 @@ for i in range(window):
 
 print(word[0:window], "→", count)
 
-# Sliding window
 for i in range(1, len(word) - window + 1):
 
     leaving_char = word[i-1]
