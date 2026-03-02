2aed5c8e62d0328dab560269ea38c3803dcc1e47
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-03-02 22:39:58 +0530

    sum of element in window

--- Week 1/Day 6/Day 6.5/sumofeverywindowofsizek.py
+++ Week 1/Day 6/Day 6.5/sumofeverywindowofsizek.py
@@ -1,8 +1,19 @@
 # sum of every window of size k
 
-nums = [2, 1, 5, 1, 3, 2]
-k = 3
+user_input = input("Enter the value and add space between them ")
+input_list = list(map(int, user_input.split()))
+k =3
+print(input_list)
+window_sum = 0
+for i in range(k):
+    window_sum += input_list[i]
 
-for i in range(len(nums) - k +1):
-    window = nums[i : k+i]
-    print(window)
+print(window_sum)
+
+for i in range(1, len(input_list) -k +1):
+    leaving = input_list[i - 1]
+    entering = input_list[i + k - 1]
+
+    window_sum = window_sum - leaving + entering
+
+    print(window_sum)
