1e7cfd05678c0b8db604add97a4ae07d058de4e0
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-03-05 23:03:31 +0530

    update

--- Week 2/Day 1/TwoSuminSortedArray.py
+++ Week 2/Day 1/TwoSuminSortedArray.py
@@ -9,13 +9,13 @@ right = len(num_list) -1
 
 while num_list[left] != num_list[right]:
 
-    sum = num_list[left] + num_list[right]
-    if sum == target:
+    current_sum = num_list[left] + num_list[right]
+    if current_sum == target:
         print(f"Sum of {num_list[left]} and {num_list[right]} is equal to target")
         break
 
-    if sum > target:
+    elif current_sum > target:
         right -= 1
 
-    if sum < target:
-        left +=1
+    else:
+        left += 1
