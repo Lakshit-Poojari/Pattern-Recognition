9811302bdfa73748dfdfe61194cc40cb5de442bb
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-03-21 16:06:26 +0530

    subarray sum k

---
+++ Week 2/Day 2/Day 2.5/SubarraySum.py
@@ -0,0 +1,18 @@
+# Subarray Sum = K
+
+nums = [1,8,6,2,5,4,8,3,7]
+k =4
+
+prefix_count = {0:1}
+count = 0
+current_sum = 0
+
+for num in nums:
+    current_sum += num
+
+    if current_sum -k in prefix_count:
+        count += prefix_count[current_sum -k]
+
+    prefix_count[current_sum] = prefix_count.get(current_sum -k, 0) +1
+
+print(count)
