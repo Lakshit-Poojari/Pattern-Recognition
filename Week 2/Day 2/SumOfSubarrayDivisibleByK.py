d1a19f90011afa84e6b095dbc46a41cc886a5c59
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-03-25 19:27:48 +0530

    subarray divisible

--- Week 2/Day 2/SumOfSubarrayDivisibleByK.py
+++ Week 2/Day 2/SumOfSubarrayDivisibleByK.py
@@ -4,9 +4,14 @@ nums = [1,8,6,2,5,4,8,3,7]
 k = 4
 
 current_sum = 0
+prefix = {0 : 1}
+count = 0
 
 for num in nums:
     current_sum += num
 
-    if current_sum % k == 0:
-        
+    if current_sum % k in prefix:
+        count += prefix[current_sum % k]
+
+    prefix[current_sum % k] = prefix.get(current_sum  % k,0) +1
+print(count)
