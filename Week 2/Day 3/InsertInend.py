b7ceff412380275f2067b235543d2c6133d79274
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-03-26 17:43:48 +0530

    insert element at end

---
+++ Week 2/Day 3/InsertInend.py
@@ -0,0 +1,18 @@
+# Insert in End
+
+nums = [1, 3, 5, 6]
+target = 7
+left = 0
+right = len(nums) - 1
+
+while left <= right:
+    mid = (left + right) // 2
+
+    if nums[mid] == target:
+        print("position is of target is ", mid)
+    elif nums[mid] < target:
+        left = mid +1
+    else:
+        right = mid -1
+
+print("Position is ", left)
