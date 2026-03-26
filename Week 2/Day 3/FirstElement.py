bb527d4756b1f6c8767e659f318ba944180fa189
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-03-26 15:57:22 +0530

    first Element

---
+++ Week 2/Day 3/FirstElement.py
@@ -0,0 +1,20 @@
+# First Element
+
+nums = [1, 2, 3, 4, 5]
+target = 1
+
+left =0 
+right = len(nums) -1
+
+while left <= right:
+    mid = (left + right)//2
+
+    if nums[mid] == target:
+        print("Position is of target is", mid)
+        break
+
+    elif nums[mid] < target:
+        left =target
+    
+    elif nums[mid] > target:
+        right = target
