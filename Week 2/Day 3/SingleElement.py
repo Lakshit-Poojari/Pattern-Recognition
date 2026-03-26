e52144d87e167ddc6c7110e00f80865103aa268f
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-03-26 11:07:32 +0530

    single element

---
+++ Week 2/Day 3/SingleElement.py
@@ -0,0 +1,21 @@
+# Single Element
+
+nums = [10]
+target = 10
+
+left = 0
+right =len(nums)-1
+
+while left <= right:
+    mid =(left +right)//2
+
+    if nums[mid] == target:
+        print("Position of target is ", mid)
+        break
+
+    elif nums[mid] < target:
+        left =target
+    
+    elif nums[mid] > target:
+        right = target
+
