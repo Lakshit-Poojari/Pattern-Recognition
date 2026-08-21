5711f97d6764074a864554abaa00336309c30683
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-08-21 12:08:40 +0530

    bank class code

---
+++ class/Classes1.py
@@ -0,0 +1,22 @@
+class Account:
+
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+
+    def debit(self, amount):
+        if amount <= self.balance:
+            self.balance = self.balance - amount
+            print("Amount", amount, "Debited from your account")
+            print("Balance is", self.balance)
+        else:
+            print("Insufficient Amount")
+
+    def credit(self, amount):
+        self.balance = self.balance + amount
+        print("Amount", amount, "Credited to your account")
+        print("Balance is", self.balance)
+
+a1 = Account("Lakshit", 5000)
+a1.credit(500)
+a1.debit(6000)
